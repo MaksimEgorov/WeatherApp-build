7d6b063297593287ed97dd4ce8a74bf43ab81b69
Maksim Egorov <42270167+MaksimEgorov@users.noreply.github.com>
2020-01-08 13:27:50 +0000

    Build

---
+++ build/precache-manifest.0f4055b0cf301f666b22a60515993337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ef6b2d99707503132d78d5a0560ccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "393d3e19493606d05734",
+    "url": "/static/css/main.1848a515.chunk.css"
+  },
+  {
+    "revision": "fd828a8c4464adf5c4aa",
+    "url": "/static/js/2.70ad52d1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.70ad52d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "393d3e19493606d05734",
+    "url": "/static/js/main.e1800579.chunk.js"
+  },
+  {
+    "revision": "26382db19aaa859b7855",
+    "url": "/static/js/runtime-main.3e364b0b.js"
+  }
+]);
